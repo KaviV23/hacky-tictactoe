ab4598de28058f9b38f5d747a3cc3d52a7f9cbe8
Kaviraj Vijayanthiran <129849114+KaviV23@users.noreply.github.com>
2025-01-05 21:57:07 +0800

    Add ScalaFX UI

--- tictactoe-client/src/main/scala/tictactoe/actors/PlayerActor.scala
+++ tictactoe-client/src/main/scala/tictactoe/actors/PlayerActor.scala
@@ -1,9 +1,12 @@
 package tictactoe.actors
 
 import akka.actor.{Actor, ActorRef, Props}
+import tictactoe.PlayerClientUI
+
 import java.util.UUID
 import tictactoe.model.Player
 import tictactoe.messages._
+
 import scala.io.StdIn.readLine
 
 class PlayerActor(player: Player, matchmaker: ActorRef) extends Actor {
@@ -12,12 +15,11 @@ class PlayerActor(player: Player, matchmaker: ActorRef) extends Actor {
       matchmaker ! JoinQueue(player.id, self)
     case GameStarted =>
       println(s"Player ${player.name} (${player.id}): Game has started!")
+      PlayerClientUI.setStatusLabelText("Game has started!")
     case RequestMove =>
       println(s"Player ${player.name} (${player.id}): It's your turn!")
-      // Prompt user for manual input
-      val row = readLine("Enter row (0, 1, or 2): ").toInt
-      val col = readLine("Enter column (0, 1, or 2): ").toInt
-      sender() ! MakeMove(player.id, row, col)
+      PlayerClientUI.setStatusLabelText("It's your turn!")
+//      sender() ! MakeMove(player.id, row, col)
     case GameResult(winner) =>
       winner match {
         case Some(symbol) => println(s"Player ${player.name}: Player $symbol wins!")
